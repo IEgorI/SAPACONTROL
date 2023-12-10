de9b9b1814c7bc0f534396641b8e97fa9751773a
Egor <you@example.com>
2023-12-10 14:10:13 +0300

    Исправил ошибки, переделал последний блок

--- src/js/rectangle.js
+++ src/js/rectangle.js
@@ -1,9 +1,9 @@
 const hover = () => {
     document.addEventListener('mouseover', function(event) {
+        var rectangle = document.getElementsByClassName("contract__rectangle")
+        var card = document.getElementsByClassName("contract__cards__card")
         if (event.target.className == "contract__cards__card"){
             var targetElement = event.target;
-            var rectangle = document.getElementsByClassName("contract__rectangle")
-            var card = document.getElementsByClassName("contract__cards__card")
             var targetElementId = targetElement.getAttribute('id');
             for (var i = 0; i < 3; i++) {
                 var cardId = card[i].getAttribute('id');
@@ -34,6 +34,54 @@ const hover = () => {
                 }
                 if (Id == "card3"){
                     rectangle[2].style.backgroundColor = '';
+                }
+            });
+        }
+        if (event.target.className == "contract__rectangle"){
+            var targetElement = event.target;
+            var targetElementId = targetElement.getAttribute('id');
+            for (var i = 0; i < 3; i++) {
+                var rectangleId = rectangle[i].getAttribute('id');
+                if (rectangleId == targetElementId){
+                    var Id = rectangleId;
+                }
+            }
+            targetElement.addEventListener('mouseenter', function(e){ // Вешаем на него обработчик mouseenter - при наведение мыши на элемент
+                e.target.style.backgroundColor = 'rgba(33, 160, 170, 0.33)'; // Выставляем цвет
+                if (Id == "rectangle1"){
+                    card[0].style.backgroundColor = '#21A0AA';
+                    card[0].style.color = 'red';
+                    card[0].children.item(1).style.color = '#FFF';
+                    card[0].children.item(0).style.color = '#FFF';
+                }
+                if (Id == "rectangle2"){
+                    card[1].style.backgroundColor = '#21A0AA';
+                    card[1].children.item(1).style.color = '#FFF';
+                    card[1].children.item(0).style.color = '#FFF';
+                }
+                if (Id == "rectangle3"){
+                    card[2].style.backgroundColor = '#21A0AA';
+                    card[2].children.item(1).style.color = '#FFF';
+                    card[2].children.item(0).style.color = '#FFF';
+                }
+            });
+            
+            targetElement.addEventListener('mouseleave', function(e){ // И ещё обработчик mouseleave - при "уходе" курсора с элемента
+                e.target.style.backgroundColor = ''; // Убираем выданный цвет\
+                if (Id == "rectangle1"){
+                    card[0].style.backgroundColor = '';
+                    card[0].children.item(1).style.color = '';
+                    card[0].children.item(0).style.color = '';
+                }
+                if (Id == "rectangle2"){
+                    card[1].style.backgroundColor = '';
+                    card[1].children.item(1).style.color = '';
+                    card[1].children.item(0).style.color = '';
+                }
+                if (Id == "rectangle3"){
+                    card[2].style.backgroundColor = '';
+                    card[2].children.item(1).style.color = '';
+                    card[2].children.item(0).style.color = '';
                 }
             });
         }
